9d387d7218229f7ee8edc6ca4000bfa50ba69f32
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-12-15 21:50:33 -0800

    Add day 16

---
+++ day16/part2.py
@@ -0,0 +1,12 @@
+inp = '11101000110010100'
+output = list(inp)
+
+while len(output) < 35651584:
+    output += ['0'] + ['0' if x == '1' else '1' for x in reversed(output)]
+output = output[0:35651584]
+
+while len(output) % 2 == 0:
+    output = ['1' if output[i] == output[i + 1] else '0' for i in range(0, len(output), 2)]
+
+print(''.join(output))
+input()
