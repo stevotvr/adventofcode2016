e61d01a2fb7d92457692c149f2d22ad65aa9cf7b
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-12-17 21:25:09 -0800

    Add day 18

---
+++ day18/part2.py
@@ -0,0 +1,14 @@
+currentRow = [x == '^' for x in open('input.txt', 'r').readline()]
+traps = ((True, True, False), (False, True, True), (True, False, False), (False, False, True))
+
+safeTiles = currentRow.count(False)
+for _ in range(399999):
+    nextRow = []
+    for i in range(len(currentRow)):
+        l, c, r = i > 0 and currentRow[i - 1], currentRow[i], i < len(currentRow) - 1 and currentRow[i + 1]
+        nextRow.append((l, c, r) in traps)
+    safeTiles += nextRow.count(False)
+    currentRow = nextRow
+
+print(safeTiles)
+input()
